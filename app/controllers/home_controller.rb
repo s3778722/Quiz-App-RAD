8c0a1a890adf33ea2e44f34a6a16d8ea7a911e12
s3778722 <s3778722@student.rmit.edu.au>
2021-06-17 18:31:18 +0800

    make it better again

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,10 @@ class HomeController < ApplicationController
   def index
     session[:marks] = 0
     session[:times] = 0
-    #url = 'https://quizapi.io/api/v1/questions?apiKey=A9ya16s39eH0hdu3BgxigMuxVa4zbjMaVjHxCOyt&limit=10'
-    #uri = URI(url)
-    #response = Net::HTTP.get(uri)
-    #result = JSON.parse(response)
-    #File.write('./quiz.json', JSON.pretty_generate(result))
+    url = 'https://quizapi.io/api/v1/questions?apiKey=A9ya16s39eH0hdu3BgxigMuxVa4zbjMaVjHxCOyt&limit=10'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    result = JSON.parse(response)
+    File.write('./quiz.json', JSON.pretty_generate(result))
   end
 end
